93c066928e3066d6ecad2758a6d51798647b554f
Chol Nhial <chol.nhial.chol@gmail.com>
2022-01-13 11:28:55 +1000

    initial commit

---
+++ src/main/java/com/cholnhial/springcli/SpringcliApplication.java
@@ -0,0 +1,13 @@
+package com.cholnhial.springcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcliApplication.class, args);
+	}
+
+}
