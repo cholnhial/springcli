0ff118c2a39f6a8a69f886574f1042dc737fe1b4
Chol Nhial <chol.nhial.chol@gmail.com>
2022-01-16 16:11:36 +1000

    add cli profile, make jar executable and test basic cli

--- src/main/java/com/cholnhial/springcli/SpringcliApplication.java
+++ src/main/java/com/cholnhial/springcli/SpringcliApplication.java
@@ -1,13 +1,27 @@
 package com.cholnhial.springcli;
 
+import com.cholnhial.springcli.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringcliApplication {
+public class SpringcliApplication implements CommandLineRunner {
+
+	@Autowired
+	private FeedbackService feedbackService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcliApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Arrays.stream(args).forEach(System.out::println);
+		var feedbacks = feedbackService.getAllFeedbacks();
+		feedbacks.forEach(f -> System.out.println(f.getFeedback()));
+	}
 }
--- src/main/java/com/cholnhial/springcli/service/FeedbackService.java
+++ src/main/java/com/cholnhial/springcli/service/FeedbackService.java
@@ -1,10 +1,22 @@
 package com.cholnhial.springcli.service;
 
+import com.cholnhial.springcli.model.Feedback;
+import com.cholnhial.springcli.repository.FeedbackRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(readOnly = true)
 @Service
+@RequiredArgsConstructor
 public class FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepository.findAll();
+    }
 
 }
